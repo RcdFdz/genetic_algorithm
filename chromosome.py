681415f41cea4b046a08e3039be14a867dc36467
RcdFdz <fernandez.domenech@hotmail.com>
2019-02-26 18:58:31 +0100

    [IMP] Interpretation of l length and first algorithm aprox

--- chromosome.py
+++ chromosome.py
@@ -16,6 +16,7 @@ class Chromosome:
         self.mutated_chromosome_str = ''
         self.mutated_chromosome_int = 0
 
+        self.crossed_chromosome_array = []
         self.crossed_chromosome_str = ''
         self.crossed_chromosome_int = 0
 
@@ -26,6 +27,7 @@ class Chromosome:
             self.chromosome_array.append(format(ch, '0' + str(self.l_length[-1]) + 'b'))
         self.chromosome_str = ''.join(self.chromosome_array)
         self.chromosome_int = int(self.chromosome_str, 2)
+        return self.chromosome_int
 
     def mutate_chromosome(self):
         for s in self.chromosome_array:
@@ -34,24 +36,35 @@ class Chromosome:
             self.mutated_chromosome_array.append(seg_mutated)
         self.mutated_chromosome_str = ''.join(self.mutated_chromosome_array)
         self.mutated_chromosome_int = int(self.mutated_chromosome_str, 2)
+        return self.mutated_chromosome_int
 
     def crossover_chromosome(self, chromosome):
-        ch1 = self.chromosome_str[:round(len(self.chromosome_str)/2)], self.chromosome_str[round(len(self.chromosome_str)/2):]
-        ch2 = chromosome[:round(len(chromosome)/2)], chromosome[round(len(chromosome)/2):]
-        ms = ch1[0] + ch2[1]
+        ch1 = chromosome[:round(len(chromosome) / 2)], chromosome[round(len(chromosome) / 2):]
+        ch2 = self.chromosome_str
+
+        len_ch1 = len(ch1[0])
+
+        ms = ch1[0] + self.chromosome_str[len_ch1:]
+
         for idx in range(0, round(len(ms)/2)):
             idx = random.choices(range(0, len(ms)))[0]
             ms = ms[:idx] + ms[idx+1:]
 
         ms_neg = ''.join([str(int(not int(i))) for i in ms])
 
-        #ch = (ch1 & Ms) | ( ch2 ∧ Ms )
+        # ch = (ch1 & Ms) | ( ch2 ∧ Ms )
         ch_ = int(''.join(ch1), 2) & int(ms, 2)
         ch__ = int(''.join(ch2), 2) & int(ms_neg, 2)
         ch___ = ch_ | ch__
-        self.crossed_chromosome_str = format(ch___, '0'+str(len(ch___))+'b')
+        self.crossed_chromosome_str = format(ch___, '0' + str(len(ch2)) + 'b')
+        len_start = 0
+        for l_len in self.l_length:
+            self.crossed_chromosome_array.append(self.crossed_chromosome_str[len_start:len_start + l_len])
+            len_start += l_len
 
-c = Chromosome(2)
-c.generate_chromosome(3, 10)
-c.mutate_chromosome()
-print(c.crossover_chromosome('10101010001011010'))
+        return self.crossed_chromosome_int
+
+    def generate_chromosomes(self):
+        choices = random.choices(range(3, 20), k=10)
+        self.generate_chromosome(min(choices), max(choices))
+        self.mutate_chromosome()
--- gkmeans.py
+++ gkmeans.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 import random
-from sklearn.cluster import KMeans
 from sklearn import preprocessing
+from chromosome import Chromosome as ch
 
 
 class GKmeans:
@@ -14,64 +14,39 @@ class GKmeans:
         self.num_nc = number_chromosomes #to be set random
 
         self.d = np.ndim(self.data)
-        self.ch_dim = self.d * 2 + 2
-
-        self.preserved_chromosome = []
-        self.mutated_chromosome = []
-        self.crossover_chromosome = []
 
     def normalize(self):
         min_max_scaler = preprocessing.MinMaxScaler()
         data_scaled = pd.DataFrame(min_max_scaler.fit_transform(self.data))
         self.data_scaled = data_scaled
 
-    def get_crossover_chromosome(self):
-        # Crossover Chromosomes
-        ch_crossover_str_list = []
-        for i in range(Nclusters):
-            ch1 = self.preserved_chromosome[random.choices(range(0,self.num_nc))[0]]
-            ch2 = self.preserved_chromosome[random.choices(range(0,self.num_nc))[0]]
-            ms = ch1 + ch2
+    def get_hyperparameters(self, ch_str_array):
 
-            for idx in range(0, round(len(ms)/2)):
-                idx = random.choices(range(0,len(ms)))[0]
-                ms= ms[:idx] + ms[idx+1:]
+        v = [int(x, 2) for x in ch_str_array]
 
-            ms_neg = ''.join([str(int(not int(i))) for i in ms])
-
-            #ch = (ch1 & Ms) | ( ch2 ∧ Ms )
-            ch_ = int(ch1,2)&int(ms,2)
-            ch__ = int(ch2,2)&int(ms_neg,2)
-            ch_crossover_str_list.append(format(ch_ | ch__, '0'+str(self.ch_dim**2)+'b'))
-        self.crossover_chromosome = ch_crossover_str_list
-        print(ch_crossover_str_list)
-
-    def get_hyperparameters(self, ch_str_list):
-        # integer to binary string format(int, '02b')
-        # binary string to integer int(binary_string, 2)
-        v = [int(x, 2) for x in list(map(''.join, zip(*[iter(ch_str_list[0])]*self.ch_dim)))]
-
-        # tri-lavel
-        wi = [v[i]/sum(v[0:d]) for i in range(0,self.d)]
-        ri = [v[i]*v[-1]/sum(v[2:-1]) for i in range(self.d,2*self.d)]
+        # tri-level
+        wi = [v[i]/sum(v[0:self.d]) for i in range(0, self.d)]
+        ri = [(v[i]*v[-1]/sum(v[2:-1])) + v[-1] for i in range(self.d, 2*self.d)]
         return wi, ri
 
-    # The following calcul return a NxM Matrix for each point in datase
-    # where:
-    # N is the number of centers
-    # M number of dimensions defining the point
+    def run(self):
+        self.normalize()
+
+        chromosomes = []
+        for nc in range(0, 3):
+            c = ch(self.d)
+            c.generate_chromosomes()
+            chromosomes.append(c)
+
+        for chrom in chromosomes:
+            self.get_hyperparameters(chrom.chromosome_array)
 
     @staticmethod
     def get_distance(data, centers, wi, ri):
-        return [np.multiply(wi, np.power(abs(np.subtract(centers, v)),ri)) for i,v in enumerate(np.matrix(data))]
+        return [np.multiply(wi, np.power(abs(np.subtract(centers, v)), ri)) for i, v in enumerate(np.matrix(data))]
 
-    #Nclusters = 3
-    #kmeans = KMeans(n_clusters=3, random_state=0).fit(self.data)
-    #centers = kmeans.cluster_centers_
-
-    #kmeans.cluster_centers_.round(4)
 
 dataset = np.array([[1,1], [1,2], [2,1], [2,2], [1,4], [1,5], [1,6], [5,6], [5,7], [6,6], [6,7]])
 
-a = GKmeans(dataset,3)
-a.get_preserved_chromosome()
+a = GKmeans(dataset, 3)
+a.run()
